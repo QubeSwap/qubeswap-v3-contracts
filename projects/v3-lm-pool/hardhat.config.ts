592f458f5b5c869963df054aab732800c9b436db
MUROKO <muroko@users.noreply.github.com>
2026-02-03 05:41:17 -0500

    update_26_4: v3-lm-pool\hardhat.config.ts

--- projects/v3-lm-pool/hardhat.config.ts
+++ projects/v3-lm-pool/hardhat.config.ts
@@ -17,23 +17,23 @@ const config: HardhatUserConfig = {
     hardhat: {},
     localhost: { timeout: 600000 },
     seiMainnet: {
-      url: https://evm-rpc.sei-apis.com,
+      url: "https://evm-rpc.sei-apis.com",
       accounts: [process.env.KEY_MAINNET!]
     },
 	monadMainnet: {
-      url: https://rpc.monad.xyz,
+      url: "https://rpc.monad.xyz",
       accounts: [process.env.KEY_MAINNET!]
     },
 	ticsMainnet: {
-      url: https://rpc.qubetics.com,
+      url: "https://rpc.qubetics.com",
       accounts: [process.env.KEY_MAINNET!]
     },
 	bscMainnet: {
-      url: https://bsc-dataseed.binance.org,
+      url: "https://bsc-dataseed.binance.org",
       accounts: [process.env.KEY_MAINNET!]
     },
 	avaxMainnet: {
-      url: https://api.avax.network/ext/bc/C/rpc,
+      url: "https://api.avax.network/ext/bc/C/rpc",
       accounts: [process.env.KEY_MAINNET!]
     },
   },	
